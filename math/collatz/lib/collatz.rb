b1c28dace3c6d970463aa3954fb235ebb8b362ea
helpotters <helpotters@penguin>
2021-10-22 21:18:00 -0700

    input Reassigns @start_num to num Argument

--- math/collatz/lib/collatz.rb
+++ math/collatz/lib/collatz.rb
@@ -5,10 +5,14 @@ require_relative "collatz/version"
 module Collatz
   # Returns the correct pathway of values for a given number
   class CollatzConjecture
-    def initialize; end
+    attr_reader :start_num
+    def initialize
+      @start_num = nil
+    end
 
     def input(num)
       raise ArgumentError.new("Not a whole number") if (num.is_a?(Integer) && (num % 1).zero?) == false
+      @start_num = num
     end
   end
 end
--- math/collatz/spec/collatz_spec.rb
+++ math/collatz/spec/collatz_spec.rb
@@ -2,10 +2,20 @@
 
 RSpec.describe Collatz do
   let(:collatz) { Collatz::CollatzConjecture.new }
-  context "input" do
+  describe "#input" do
     it "only allows whole numbers as an input" do
       expect { collatz.input("asdf") }.to raise_error(ArgumentError)
       expect { collatz.input(1234.12) }.to raise_error(ArgumentError)
+      expect { collatz.input(12) }.to_not raise_error
     end
+
+    it "sets the starting number as an instance variable" do
+      collatz.input(13)
+      expect(collatz.start_num).to eq(13)
+    end
+  end
+
+  context "tree building" do
+
   end
 end
